a9fa88326dca0921e50c507cc480718ee9b4174e
Amirxon99 <114159472+Amirxon99@users.noreply.github.com>
2025-04-16 19:30:19 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  
+  return (
+     <>
+     <div className='container'>
+     <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente quas porro accusantium. Laboriosam corporis sapiente quam, cum voluptates libero facere, veniam doloribus repudiandae culpa eos deserunt quasi commodi architecto possimus!</p>
+    <ul>
+       <li>Html</li>
+       <li>Html</li>
+       <li>Html</li>
+       <li>Html</li>
+    </ul>
+     </div>
+
+    </> 
+      
+
+  )
+}
+
+export default App
